bf30ee4de803204ee54cc7365510aa2485bddb16
PiyumalKK <piyumalm912@gmail.com>
2024-12-03 09:12:37 +0530

    Order Implementation

--- lib/pages/order_payment/delivery_address.dart
+++ lib/pages/order_payment/delivery_address.dart
@@ -1,8 +1,4 @@
-// ignore_for_file: library_private_types_in_public_api
-
 import 'package:flutter/material.dart';
-
-// My Own Imports
 import 'package:go_kart/pages/order_payment/payment.dart';
 import 'package:page_transition/page_transition.dart';
 
@@ -14,6 +10,11 @@ class Delivery extends StatefulWidget {
 }
 
 class _DeliveryState extends State<Delivery> {
+  final TextEditingController pinCodeController = TextEditingController();
+  final TextEditingController localityController = TextEditingController();
+  final TextEditingController cityController = TextEditingController();
+  final TextEditingController stateController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
@@ -34,7 +35,7 @@ class _DeliveryState extends State<Delivery> {
               crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
                 const Text(
-                  'Where are your Ordered Item Shipped?',
+                  'Where are your Ordered Items Shipped?',
                   style: TextStyle(
                       fontSize: 30.0,
                       color: Colors.grey,
@@ -45,8 +46,9 @@ class _DeliveryState extends State<Delivery> {
                 ),
                 SizedBox(
                   width: width - 40.0,
-                  child: const TextField(
-                    decoration: InputDecoration(
+                  child: TextField(
+                    controller: pinCodeController,
+                    decoration: const InputDecoration(
                       labelText: 'Pin Code',
                       labelStyle: TextStyle(
                         fontWeight: FontWeight.bold,
@@ -55,13 +57,12 @@ class _DeliveryState extends State<Delivery> {
                     ),
                   ),
                 ),
-                const SizedBox(
-                  height: 18.0,
-                ),
+                const SizedBox(height: 18.0),
                 SizedBox(
                   width: width - 40.0,
-                  child: const TextField(
-                    decoration: InputDecoration(
+                  child: TextField(
+                    controller: localityController,
+                    decoration: const InputDecoration(
                       labelText: 'Locality',
                       labelStyle: TextStyle(
                         fontWeight: FontWeight.bold,
@@ -70,13 +71,12 @@ class _DeliveryState extends State<Delivery> {
                     ),
                   ),
                 ),
-                const SizedBox(
-                  height: 18.0,
-                ),
+                const SizedBox(height: 18.0),
                 SizedBox(
                   width: width - 40.0,
-                  child: const TextField(
-                    decoration: InputDecoration(
+                  child: TextField(
+                    controller: cityController,
+                    decoration: const InputDecoration(
                       labelText: 'City',
                       labelStyle: TextStyle(
                         fontWeight: FontWeight.bold,
@@ -85,13 +85,12 @@ class _DeliveryState extends State<Delivery> {
                     ),
                   ),
                 ),
-                const SizedBox(
-                  height: 18.0,
-                ),
+                const SizedBox(height: 18.0),
                 SizedBox(
                   width: width - 40.0,
-                  child: const TextField(
-                    decoration: InputDecoration(
+                  child: TextField(
+                    controller: stateController,
+                    decoration: const InputDecoration(
                       labelText: 'State',
                       labelStyle: TextStyle(
                         fontWeight: FontWeight.bold,
@@ -100,19 +99,24 @@ class _DeliveryState extends State<Delivery> {
                     ),
                   ),
                 ),
-                const SizedBox(
-                  height: 40.0,
-                ),
+                const SizedBox(height: 40.0),
                 Material(
                   elevation: 5.0,
                   borderRadius: BorderRadius.circular(30.0),
                   child: InkWell(
                     onTap: () {
                       Navigator.push(
-                          context,
-                          PageTransition(
-                              type: PageTransitionType.rightToLeft,
-                              child: const PaymentPage()));
+                        context,
+                        PageTransition(
+                          type: PageTransitionType.rightToLeft,
+                          child: PaymentPage(
+                            pinCode: pinCodeController.text,
+                            locality: localityController.text,
+                            city: cityController.text,
+                            state: stateController.text,
+                          ),
+                        ),
+                      );
                     },
                     child: Container(
                       width: width - 40.0,
--- lib/pages/order_payment/payment.dart
+++ lib/pages/order_payment/payment.dart
@@ -1,12 +1,21 @@
-// ignore_for_file: library_private_types_in_public_api
-
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-
-// My Own Imports
 import 'package:go_kart/pages/home.dart';
 
 class PaymentPage extends StatefulWidget {
-  const PaymentPage({super.key});
+  final String pinCode;
+  final String locality;
+  final String city;
+  final String state;
+
+  const PaymentPage({
+    super.key,
+    required this.pinCode,
+    required this.locality,
+    required this.city,
+    required this.state,
+  });
 
   @override
   _PaymentPageState createState() => _PaymentPageState();
@@ -24,6 +33,89 @@ class _PaymentPageState extends State<PaymentPage> {
   setSelectedRadioPayment(int val) {
     setState(() {
       selectedRadioPayment = val;
+    });
+  }
+
+  Future<void> _saveOrderToFirestore() async {
+    try {
+      User? user = FirebaseAuth.instance.currentUser;
+
+      if (user == null) {
+        throw Exception("User is not signed in.");
+      }
+
+      final orderData = {
+        'shippingAddress': {
+          'pinCode': widget.pinCode,
+          'locality': widget.locality,
+          'city': widget.city,
+          'state': widget.state,
+        },
+        'paymentMethod': selectedRadioPayment == 1
+            ? 'Credit / Debit Card'
+            : 'Cash On Delivery',
+        'orderDate': FieldValue.serverTimestamp(),
+        'orderStatus': 'Pending',
+      };
+
+      await FirebaseFirestore.instance
+          .collection('users')
+          .doc(user.uid)
+          .collection('orders')
+          .add(orderData);
+
+      print("Order successfully added to Firestore!");
+    } catch (e) {
+      print("Error saving order: $e");
+    }
+  }
+
+  void _showDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return Dialog(
+          elevation: 5.0,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
+          child: SizedBox(
+            height: 250.0,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  width: 100.0,
+                  height: 100.0,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50.0),
+                    color: Theme.of(context).primaryColor,
+                  ),
+                  child:
+                      const Icon(Icons.check, color: Colors.white, size: 50.0),
+                ),
+                const SizedBox(height: 20.0),
+                const Text(
+                  "Yuppy !!",
+                  style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 20.0),
+                const Text(
+                  "Your Payment Received.",
+                  style: TextStyle(fontSize: 18.0, color: Colors.grey),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+
+    Future.delayed(const Duration(milliseconds: 1000), () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => const Home()),
+      );
     });
   }
 
@@ -56,12 +148,9 @@ class _PaymentPageState extends State<PaymentPage> {
                       height: 1.6),
                   textAlign: TextAlign.center,
                 ),
-                const SizedBox(
-                  height: 20.0,
-                ),
+                const SizedBox(height: 20.0),
                 Container(
                   width: width - 40.0,
-                  margin: const EdgeInsets.only(top: 10.0, bottom: 10.0),
                   child: RadioListTile(
                     value: 1,
                     groupValue: selectedRadioPayment,
@@ -74,21 +163,11 @@ class _PaymentPageState extends State<PaymentPage> {
                       setSelectedRadioPayment(val!);
                     },
                     activeColor: Colors.blue,
-                    secondary: const Image(
-                      image: AssetImage(
-                        'assets/payment_icon/card.png',
-                      ),
-                      height: 45.0,
-                      width: 45.0,
-                    ),
                   ),
                 ),
-                const Divider(
-                  height: 1.0,
-                ),
+                const Divider(height: 1.0),
                 Container(
                   width: width - 40.0,
-                  margin: const EdgeInsets.only(top: 10.0, bottom: 10.0),
                   child: RadioListTile(
                     value: 2,
                     groupValue: selectedRadioPayment,
@@ -101,29 +180,15 @@ class _PaymentPageState extends State<PaymentPage> {
                       setSelectedRadioPayment(val!);
                     },
                     activeColor: Colors.blue,
-                    secondary: const Image(
-                      image: AssetImage(
-                        'assets/payment_icon/cash_on_delivery.png',
-                      ),
-                      height: 45.0,
-                      width: 45.0,
-                    ),
                   ),
                 ),
-                const Divider(
-                  height: 1.0,
-                ),
-                const Divider(
-                  height: 1.0,
-                ),
-                const SizedBox(
-                  height: 40.0,
-                ),
+                const SizedBox(height: 40.0),
                 Material(
                   elevation: 5.0,
                   borderRadius: BorderRadius.circular(30.0),
                   child: InkWell(
                     onTap: () {
+                      _saveOrderToFirestore();
                       _showDialog();
                     },
                     child: Container(
@@ -135,7 +200,7 @@ class _PaymentPageState extends State<PaymentPage> {
                         borderRadius: BorderRadius.circular(30.0),
                       ),
                       child: const Text(
-                        'Pay',
+                        'Confirm Payment',
                         style: TextStyle(
                             color: Colors.white,
                             fontSize: 18.0,
@@ -150,71 +215,5 @@ class _PaymentPageState extends State<PaymentPage> {
         ],
       ),
     );
-  }
-
-  // user defined function for Logout Dialogue
-  void _showDialog() {
-    // flutter defined function
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        // return object of type Dialog
-        return Dialog(
-          elevation: 5.0,
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
-          child: SizedBox(
-            height: 250.0,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                  width: 100.0,
-                  height: 100.0,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(50.0),
-                    color: Theme.of(context).primaryColor,
-                    border: Border.all(
-                        color: Theme.of(context).primaryColorLight, width: 5.0),
-                  ),
-                  child: const Icon(
-                    Icons.check,
-                    color: Colors.white,
-                    size: 50.0,
-                  ),
-                ),
-                const SizedBox(
-                  height: 20.0,
-                ),
-                const Text(
-                  "Yuppy !!",
-                  style: TextStyle(
-                    fontSize: 25.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const SizedBox(
-                  height: 20.0,
-                ),
-                const Text(
-                  "Your Payment Received.",
-                  style: TextStyle(fontSize: 18.0, color: Colors.grey),
-                ),
-              ],
-            ),
-          ),
-        );
-      },
-    );
-
-    Future.delayed(const Duration(milliseconds: 1000), () {
-      setState(() {
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => const Home()),
-        );
-      });
-    });
   }
 }
